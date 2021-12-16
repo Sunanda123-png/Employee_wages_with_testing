822bca0ab39d31f25aa7358ffc20d22063f7bbb7
Sunanda123-png <sunanda02@gmail.com>
2021-12-16 15:57:14 +0530

    ADD: Test cases for employee class

--- employee_test.py
+++ employee_test.py
@@ -2,15 +2,27 @@ from employee_wages import Employee
 
 
 def test_check_attendence():
+    """
+    testing the check attandence function
+    :return:
+    """
     e1 = Employee("sunanda", 20, 14, 10)
     assert e1.check_attendence() == 2
 
 
 def test_attendence():
+    """
+    testing the attandence function
+    :return:
+    """
     e2 = Employee("sunanda", 20, 14, 10)
     assert e2.attendence() == 0
 
 
 def test_calculate_wages():
+    """
+    testing the calculate wages function
+    :return:
+    """
     e3 = Employee("sunanda", 20, 14, 10)
     assert e3.calculate_daily_wage() == 0
