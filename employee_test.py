37fbe6959a9320bb7e0ace216f02269b536a2b11
Sunanda123-png <sunanda02@gmail.com>
2021-12-22 00:26:40 +0530

    Add: Test cases for all method in employee wage

--- employee_test.py
+++ employee_test.py
@@ -1,4 +1,5 @@
 from employee_wages import Employee
+from employee_wages import Company
 
 
 def test_check_attendence():
@@ -6,8 +7,8 @@ def test_check_attendence():
     testing the check attandence function
     :return:
     """
-    e1 = Employee("sunanda", 20, 14, 10)
-    assert e1.check_attendence() == 2
+    e1 = Employee("sunanda", 20, 14, 10, 5)
+    assert (e1.check_attendence() == 2 or e1.check_attendence() == 1 or e1.check_attendence() == 0)
 
 
 def test_attendence():
@@ -15,8 +16,8 @@ def test_attendence():
     testing the attandence function
     :return:
     """
-    e2 = Employee("sunanda", 20, 14, 10)
-    assert e2.attendence() == 0
+    e2 = Employee("sunanda", 20, 14, 10,10)
+    assert isinstance(e2, Employee) == True
 
 
 def test_calculate_wages():
@@ -24,5 +25,21 @@ def test_calculate_wages():
     testing the calculate wages function
     :return:
     """
-    e3 = Employee("sunanda", 20, 14, 10)
-    assert e3.calculate_daily_wage() == 0
+    e3 = Employee("Sunanda", 20, 14, 10, 5)
+    assert (e3.check_attendence() == 2 or e3.check_attendence() == 1 or e3.check_attendence() == 0)
+
+
+def test_inserting_employee():
+    """
+    testing the inserting employee function
+    :return:
+    """
+    c1 = Company("Dmart")
+    e3 = Employee("Sunanda", 20, 14, 10, 5)
+    c1.add_employee(e3)
+    assert len(c1.employee_list) > 0
+
+
+def test_get_company():
+    c1 = Company("Dmart")
+    assert isinstance(c1,Company) == True
